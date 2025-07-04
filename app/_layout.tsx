e244b71b6f104b4ee83b00c2a648782bde87229a
Anum Tariq <80879547+aanumtariq@users.noreply.github.com>
2025-07-04 12:45:14 +0500

    remove commented code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,3 @@
-// import { useEffect } from 'react';
-// import { Stack } from 'expo-router';
-// import { StatusBar } from 'expo-status-bar';
-// import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-// import { AuthProvider } from '@/context/AuthContext';
-// import { ThemeProvider } from '@/context/ThemeContext';
-// import { GestureHandlerRootView } from 'react-native-gesture-handler';
-// // import { MenuProvider } from 'react-native-popup-menu';
-
-// export default function RootLayout() {
-//   useFrameworkReady();
-
-//   return (
-//     <GestureHandlerRootView style={{ flex: 1 }}>
-//     {/* <MenuProvider> */}
-
-//     <ThemeProvider>
-//       <AuthProvider>
-//         <Stack screenOptions={{ headerShown: false }}>
-//           <Stack.Screen name="index" />
-//           <Stack.Screen name="welcome" />
-//           <Stack.Screen name="(auth)" />
-//           <Stack.Screen name="(tabs)" />
-//           <Stack.Screen name="group-create" />
-//           <Stack.Screen name="group-chat/[id]" />
-//           <Stack.Screen name="+not-found" />
-//         </Stack>
-//         <StatusBar style="auto" />
-//       </AuthProvider>
-//     </ThemeProvider>
-//     {/* </MenuProvider> */}
-//     </GestureHandlerRootView>
-//   );
-// }
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
